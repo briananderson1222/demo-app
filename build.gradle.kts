e6df848b59801229c93cb65906c2bbf7ab2ba066
Brian Anderson <brian@briananderson.xyz>
2024-02-11 14:51:05 -0700

    Attempt to use dynamic pipeline definitions, add jacoco to sonar coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "app"
@@ -22,4 +23,20 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "briananderson1222_demo-app")
+    property("sonar.organization", "briananderson1222")
+    property("sonar.host.url", "https://sonarcloud.io")
+	property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco")
+  }
+}
+
+jacocoTestReport {
+    reports {
+        xml.enabled = true // Enable XML report generation
+        xml.required = true // Require XML report to be generated
+    }
 }
